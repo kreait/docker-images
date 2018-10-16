a09f31312faedef6fb6f366741592f0ad02c4d0e
Michael Krieg <info@red-tag.de>
2018-10-16 16:48:40 +0200

    Add php7-gd and php7-zip (#2)

--- php/scripts/install-packages.sh
+++ php/scripts/install-packages.sh
@@ -3,12 +3,14 @@ set -ex
 
 apk --no-cache add \
     bash \
+    zip unzip \
     php7 \
     php7-ctype \
     php7-curl \
     php7-dom \
     php7-fileinfo \
     php7-ftp \
+    php7-gd \
     php7-iconv \
     php7-json \
     php7-mbstring \
@@ -26,4 +28,5 @@ apk --no-cache add \
     php7-xml \
     php7-xmlreader \
     php7-xmlwriter \
+    php7-zip \
     php7-zlib
